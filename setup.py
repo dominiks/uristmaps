6891949d86dd836caabf5328d4d25a778cf91c2f
domschacht <domschacht@gmail.com>
2014-08-26 11:52:32 +0200

    basic setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name="UristMaps",
+      version="0.1",
+      author="Dominik Schacht",
+      author_email="domschacht@gmail.com",
+      description="Map renderer for Dwarf Fortress worlds.",
+      url="http://www.bitbucket.org/dominiks/uristmap",
+      install_requires=["pillow",        # Image processing
+                        "cairocffi",     # Drawing tile images
+                        "clint",         # Pretty cli output
+                        "doit",          # Quick management of tasks and execution order
+                        "beautifulsoup4" # Processing xml
+     ]
+)
+
