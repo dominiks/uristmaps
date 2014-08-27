6ccffa29819932d33bbeb08541db04f725d944f5
domschacht <domschacht@gmail.com>
2014-08-27 12:28:08 +0200

    added error message and app exit when conf file is not found

--- uristmaps/config.py
+++ uristmaps/config.py
@@ -1,7 +1,14 @@
+import os, sys
 import configparser
 
 from doit import get_var
 
 conf = configparser.ConfigParser()
-conf.read(get_var("conf", "config.cfg"))
+
+conf_path = get_var("conf", "config.cfg")
+if not os.path.exists(conf_path):
+    print("Config file '{}' not found!.".format(conf_path))
+    sys.exit(1)
+
+conf.read(conf_path)
 
