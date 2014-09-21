956aeb5507c6d32a254a64c146c5c87857899601
Dominik Schacht <domschacht@gmail.com>
2014-09-21 20:22:09 +0200

    what's that dot doing there?

--- uristmaps/config.py
+++ uristmaps/config.py
@@ -7,7 +7,7 @@ conf = configparser.ConfigParser()
 
 conf_path = get_var("conf", "config.cfg")
 if not os.path.exists(conf_path):
-    print("Config file '{}' not found!.".format(conf_path))
+    print("Config file '{}' not found!".format(conf_path))
     sys.exit(1)
 
 conf.read(conf_path)
